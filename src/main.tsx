fe2a291bb8d3f994b7b959cd738156a4ca4c416f
aryoyudhanto <aryoyudhanto26399@gmail.com>
2023-03-15 11:37:54 +0700

    build: install and implement react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import "./styles/index.css";
+import App from "./routes";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <App />
+);
